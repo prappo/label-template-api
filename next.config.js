a8bc96dd87df5baddb9ec12b5b6c81c5e06f7ca1
Prappo Prince <prappo.prince@gmail.com>
2023-01-01 17:24:38 +0600

    fix cors issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  crossOrigin: 'anonymous'
 }
 
 module.exports = nextConfig
