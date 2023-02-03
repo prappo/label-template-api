4dcd3376ec537efce8781f7d9a42cf1ea7455c9f
Prappo <prappo.prince@gmail.com>
2023-02-03 17:25:52 +0000

    few update

--- generate_template.js
+++ generate_template.js
@@ -20,15 +20,22 @@ function addTemplateToCategory(categories) {
     categories.forEach(cat => {
         let catPath = './public/templates/' + cat
         let c = require(catPath + '/index.json');
-
+        
         let templatesFolder = getDirectories(catPath + '/templates')
-        templatesFolder.forEach(folder => {
-            let templatePath = catPath + '/' + folder + '/template.json';
-            console.log(templatePath)
-          
-            //    let templateData =  JSON.parse(fs.readFileSync(templatePath));
-            //    console.log(templateData.props)
-        })
+        for( index in c){
+            let templatePath = catPath + '/' + c[index].path
+            let templateData = require(templatePath);
+            c[index].props = templateData.props;
+        }
+        
+        // templatesFolder.forEach(folder => {
+        //     let templatePath = catPath + '/templates/' + folder + '/template.json';
+        //     console.log(templatePath)
+        //     let templateData = require(templatePath);
+            
+        //     // c.props = templateData.props
+        //     //    console.log(templateData.props)
+        // })
         templatesData[cat] = c
     })
 
@@ -36,18 +43,19 @@ function addTemplateToCategory(categories) {
 }
 
 let templates = addTemplateToCategory(templatesCat);
-// let allTemplates = []
-// // console.log(templates);
-// for(let cat in templates){
-//     // console.log(templates[cat])
-//     templates[cat].forEach(temp => {
-//         temp.path = cat + '/' + temp.path
-//         temp.preview = cat + '/' + temp.preview
-//         temp.props = temp.props
-//        allTemplates.push(temp)
-//     })
-// }
-// fs.writeFileSync('./public/templates/index.json', JSON.stringify( allTemplates ));
-// console.log(allTemplates)
+let allTemplates = []
+// console.log(templates);
+for(let cat in templates){
+    // console.log(templates[cat])
+    templates[cat].forEach(temp => {
+        temp.path = cat + '/' + temp.path
+        temp.preview = cat + '/' + temp.preview
+        temp.props = temp.props
+       allTemplates.push(temp)
+    })
+}
+fs.writeFileSync('./public/templates/index.json', JSON.stringify( allTemplates ));
+console.log(allTemplates)
 
 // console.log(templates)
+
