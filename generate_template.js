cf8d080746308bcee5c70f3eb0ac4f8e919ac6f8
Prappo Prince <prappo.prince@gmail.com>
2023-02-03 20:28:11 +0600

    few udpate

--- generate_template.js
+++ generate_template.js
@@ -6,32 +6,48 @@ function getDirectories(path) {
     });
 }
 
+// Get categories
 let templatesCat = []
 getDirectories('public/templates').forEach(dir => {
     templatesCat.push(dir)
 })
 
+
+
+// Get template category file data
 let templatesData = []
 function addTemplateToCategory(categories) {
     categories.forEach(cat => {
-       let c = require('./public/templates/' + cat + '/index.json');
-       templatesData[cat] = c
+        let catPath = './public/templates/' + cat
+        let c = require(catPath + '/index.json');
+
+        let templatesFolder = getDirectories(catPath + '/templates')
+        templatesFolder.forEach(folder => {
+            let templatePath = catPath + '/' + folder + '/template.json';
+            console.log(templatePath)
+          
+            //    let templateData =  JSON.parse(fs.readFileSync(templatePath));
+            //    console.log(templateData.props)
+        })
+        templatesData[cat] = c
     })
 
     return templatesData;
 }
 
 let templates = addTemplateToCategory(templatesCat);
-let allTemplates = []
-// console.log(templates);
-for(let cat in templates){
-    // console.log(templates[cat])
-    templates[cat].forEach(temp => {
-        temp.path = cat + '/' + temp.path
-        temp.preview = cat + '/' + temp.preview
-       allTemplates.push(temp)
-    })
-}
-fs.writeFileSync('./public/templates/index.json', JSON.stringify( allTemplates ));
-console.log(allTemplates)
+// let allTemplates = []
+// // console.log(templates);
+// for(let cat in templates){
+//     // console.log(templates[cat])
+//     templates[cat].forEach(temp => {
+//         temp.path = cat + '/' + temp.path
+//         temp.preview = cat + '/' + temp.preview
+//         temp.props = temp.props
+//        allTemplates.push(temp)
+//     })
+// }
+// fs.writeFileSync('./public/templates/index.json', JSON.stringify( allTemplates ));
+// console.log(allTemplates)
 
+// console.log(templates)
